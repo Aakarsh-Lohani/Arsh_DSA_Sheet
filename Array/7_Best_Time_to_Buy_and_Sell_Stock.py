1c8e5853608a242175e926accd7f6656e6bd59ef
AAKARSH LOHANI <aakarshlohani01@gmail.com>
2024-02-22 00:07:46 +0530

    solving array

--- Array/7_Best_Time_to_Buy_and_Sell_Stock.py
+++ Array/7_Best_Time_to_Buy_and_Sell_Stock.py
@@ -1,5 +1,20 @@
 #https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
 #121. Best Time to Buy and Sell Stock
+""" 
+#Same approach #my 
+
+from typing import List 
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        min=prices[0]
+        profit=0
+        for price in prices:
+            if price<min:
+                min=price
+            elif price>min and price-min>profit:
+                profit=price-min
+        return profit
+"""
 from typing import List
 
 class Solution:
