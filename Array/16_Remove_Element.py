f39ec3aedd28b2c340d09202e6129b16c50fc832
AAKARSH LOHANI <aakarshlohani01@gmail.com>
2024-03-09 23:57:22 +0530

    changing nums in place

---
+++ Array/16_Remove_Element.py
@@ -0,0 +1,27 @@
+# https://leetcode.com/problems/remove-element/description/
+# 27. Remove Element
+
+#Both approach beats around 50%
+from typing import List
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        i = 0
+        for j in range(len(nums)):
+            if nums[j] != val:
+                nums[i] = nums[j]
+                i += 1
+        return i
+    
+
+from typing import List
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        i = 0
+        n = len(nums)
+        while i < n:
+            if nums[i] == val:
+                nums[i] = nums[n - 1]
+                n -= 1
+            else:
+                i += 1
+        return n
