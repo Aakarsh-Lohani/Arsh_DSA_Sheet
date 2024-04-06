a9fbd78167b61ba9e67a7003f7cbe1124584aa54
AAKARSH LOHANI <aakarshlohani01@gmail.com>
2024-04-06 23:13:47 +0530

    Time Complexity : O(2^n)

---
+++ Backtracking/03_Subsets_II.py
@@ -0,0 +1,21 @@
+https://leetcode.com/problems/subsets-ii/description/
+90. Subsets II
+
+
+class Solution:
+    def subsetsWithDup(self, nums):
+        nums.sort()  # Sorting input array to handle duplicates
+        
+        def backtrack(start, path, result):
+            result.append(path)
+            
+            for i in range(start, len(nums)):
+                
+                if i > start and nums[i] == nums[i-1]:
+                    continue
+                
+                backtrack(i + 1, path + [nums[i]], result)
+        
+        result = []
+        backtrack(0, [], result)
+        return result
