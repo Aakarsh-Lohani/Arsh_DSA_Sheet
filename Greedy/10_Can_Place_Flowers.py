98c3a2937bc0da4289d796ab0ae45d1e787f89d1
AAKARSH LOHANI <aakarshlohani01@gmail.com>
2024-04-25 00:00:03 +0530

    optimized solution

--- Greedy/10_Can_Place_Flowers.py
+++ Greedy/10_Can_Place_Flowers.py
@@ -12,3 +12,16 @@ class Solution:
                 flowerbed[i] = 1
             i += 1
         return rem == 0
+    
+# optimized solution
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        i, count = 0, 0
+        while i < len(flowerbed):
+            if flowerbed[i] == 0 and (i == 0 or flowerbed[i - 1] == 0) and (i == len(flowerbed) - 1 or flowerbed[i + 1] == 0):
+                flowerbed[i] = 1
+                count += 1
+                i += 2  # skip the next plot
+            else:
+                i += 1  # move to the next plot
+        return count >= n
