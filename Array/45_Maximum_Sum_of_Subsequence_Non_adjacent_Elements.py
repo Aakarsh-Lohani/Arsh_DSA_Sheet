bca0a4df8ec68b86451d7acb7fb2e6db4cbfd8cf
AAKARSH LOHANI <aakarshlohani01@gmail.com>
2024-05-28 23:51:31 +0530

    Weekly Contest 399

---
+++ Array/45_Maximum_Sum_of_Subsequence_Non_adjacent_Elements.py
@@ -0,0 +1,139 @@
+# https://leetcode.com/problems/maximum-sum-of-subsequence-with-non-adjacent-elements/
+# 3165. Maximum Sum of Subsequence With Non-adjacent Elements
+
+import typing
+from typing import List
+class SegTree:
+    def __init__(self,
+                 op: typing.Callable[[typing.Any, typing.Any], typing.Any],
+                 e: typing.Any,
+                 v: typing.Union[int, typing.List[typing.Any]]) -> None:
+        self._op = op
+        self._e = e
+
+        if isinstance(v, int):
+            v = [e] * v
+
+        self._n = len(v)
+        self._log = (self._n-1).bit_length()
+        self._size = 1 << self._log
+        self._d = [e] * (2 * self._size)
+
+        for i in range(self._n):
+            self._d[self._size + i] = v[i]
+        for i in range(self._size - 1, 0, -1):
+            self._update(i)
+
+    def set(self, p: int, x: typing.Any) -> None:
+        assert 0 <= p < self._n
+
+        p += self._size
+        self._d[p] = x
+        for i in range(1, self._log + 1):
+            self._update(p >> i)
+
+    def get(self, p: int) -> typing.Any:
+        assert 0 <= p < self._n
+
+        return self._d[p + self._size]
+
+    def prod(self, left: int, right: int) -> typing.Any:
+        assert 0 <= left <= right <= self._n
+        sml = self._e
+        smr = self._e
+        left += self._size
+        right += self._size
+
+        while left < right:
+            if left & 1:
+                sml = self._op(sml, self._d[left])
+                left += 1
+            if right & 1:
+                right -= 1
+                smr = self._op(self._d[right], smr)
+            left >>= 1
+            right >>= 1
+
+        return self._op(sml, smr)
+
+    def all_prod(self) -> typing.Any:
+        return self._d[1]
+
+    def max_right(self, left: int,
+                  f: typing.Callable[[typing.Any], bool]) -> int:
+        assert 0 <= left <= self._n
+        assert f(self._e)
+
+        if left == self._n:
+            return self._n
+
+        left += self._size
+        sm = self._e
+
+        first = True
+        while first or (left & -left) != left:
+            first = False
+            while left % 2 == 0:
+                left >>= 1
+            if not f(self._op(sm, self._d[left])):
+                while left < self._size:
+                    left *= 2
+                    if f(self._op(sm, self._d[left])):
+                        sm = self._op(sm, self._d[left])
+                        left += 1
+                return left - self._size
+            sm = self._op(sm, self._d[left])
+            left += 1
+
+        return self._n
+
+    def min_left(self, right: int,
+                 f: typing.Callable[[typing.Any], bool]) -> int:
+        assert 0 <= right <= self._n
+        assert f(self._e)
+
+        if right == 0:
+            return 0
+
+        right += self._size
+        sm = self._e
+
+        first = True
+        while first or (right & -right) != right:
+            first = False
+            right -= 1
+            while right > 1 and right % 2:
+                right >>= 1
+            if not f(self._op(self._d[right], sm)):
+                while right < self._size:
+                    right = 2 * right + 1
+                    if f(self._op(self._d[right], sm)):
+                        sm = self._op(self._d[right], sm)
+                        right -= 1
+                return right + 1 - self._size
+            sm = self._op(self._d[right], sm)
+
+        return 0
+
+    def _update(self, k: int) -> None:
+        self._d[k] = self._op(self._d[2 * k], self._d[2 * k + 1])
+
+def op(x,y):
+    b1,l1,r1,a1=x
+    b2,l2,r2,a2=y
+    b=max(b1+r2,l1+b2)
+    l=max(b1+a2,l1+l2)
+    r=max(a1+b2,r1+r2)
+    a=max(a1+l2,r1+a2)
+    return (b,l,r,a)
+
+class Solution:
+    def maximumSumSubsequence(self, nums: List[int], queries: List[List[int]]) -> int:
+        md=10**9+7
+        st=SegTree(op,(0,0,0,0),[(0,0,0,max(0,a)) for a in nums])
+        ans=0
+        for p,a in queries:
+            st.set(p,(0,0,0,max(0,a)))
+            ans+=st.all_prod()[3]
+            ans%=md
+        return ans
